d680888430bf1b42d6f28762f2ced0d38456fd3f
Abhiram98 <bellur.abhiram@gmail.com>
2024-07-06 19:09:57 -0600

    add parameter to input mapping

--- pybrary_extraction/lisp2py/StitchAbstraction.py
+++ pybrary_extraction/lisp2py/StitchAbstraction.py
@@ -32,9 +32,23 @@ class StitchUse:
         rewrite_target = lisp2py.Rewrite2Py(target, available_abstractions=[], string_hashmap=self.string_hashmap)
         self.target_py = rewrite_target.convert()
         self.target_ast: ast.Module = rewrite_target.converted_ast
+        self.parameter_map = {} # map of param_name -> value passes
+
+    def set_params_map_from_stitch_params(self, stitch_params: set[StitchParam]):
+        func_call: ast.Call = self.application_ast.body[-1].value
+        for param in stitch_params:
+            self.parameter_map[param.param_name] = \
+                func_call.args[param.position]
+
+    def get_param_map_filtered_by_names(self):
+        new_map = {}
+        for param in self.parameter_map:
+            if isinstance(self.parameter_map[param], ast.Name):
+                new_map[self.parameter_map[param].id] = param
+        return new_map
 
     def get_application_param_from_number(self, param_number)-> ast.Expr:
-        func_call: ast.Call = self.application_ast.body[1].value
+        func_call: ast.Call = self.application_ast.body[-1].value
         assert isinstance(func_call, ast.Call)
         if param_number >= len(func_call.args):
             raise Exception(f"param_number {param_number}>={len(func_call.args)}")
@@ -74,27 +88,46 @@ class StitchAbstraction:
 
         live_vars_out = set()
         for use in self.uses_py:
-            target = use.target_py
-            for py_orig in py_originals:
-                index = py_orig.find(target)
-                if index != -1:
-                    # Found the code where the method was extracted.
-                    live_vars = lisp2py.ExtractedFragment \
-                        .create_from(py_orig, target, index).find_used_vars_later()
-                    live_vars_out = live_vars_out.union(live_vars)
+            use.set_params_map_from_stitch_params(self.parameters)
+            live_vars_out = self.find_live_vars_from_extracted_fragment(py_originals, use)
+            live_vars_out = live_vars_out.union(
+                self.find_live_vars_from_trailing_statements(use))
+            name_map = use.get_param_map_filtered_by_names()
+            if intersecting := live_vars_out.intersection(set(name_map.keys())):
+                live_vars_out = live_vars_out.union({name_map[i] for i in intersecting})
 
-                    read_vars = FindReadVariables()
-                    for trailing_param in self.get_trailing_statement_params():
-                        # run liveness on trailing statements.
-                        trailing_statements = use.get_application_param_from_number(trailing_param.position)
-                        if isinstance(trailing_statements, list):
-                            trailing_statements = ast.Module(body=trailing_statements)
-                        elif trailing_statements == python2lisp.Py2Lisp.empty_statement_keyword:
-                            continue
-                        read_vars.visit(trailing_statements)
-                        live_vars_out = live_vars_out.union(set(read_vars.rhs_vars))
+
 
         self.live_vars_out = self.live_vars_out.union(live_vars_out)
+        return live_vars_out
+
+    def find_live_vars_from_trailing_statements(self, use):
+        live_vars_out = set()
+        read_vars = FindReadVariables()
+        for trailing_param in self.get_trailing_statement_params():
+            # run liveness on trailing statements.
+            trailing_statements = use.get_application_param_from_number(trailing_param.position)
+            if isinstance(trailing_statements, list):
+                trailing_statements = ast.Module(body=trailing_statements)
+            elif trailing_statements == python2lisp.Py2Lisp.empty_statement_keyword:
+                continue
+            read_vars.visit(trailing_statements)
+            live_vars_out = live_vars_out.union(set(read_vars.rhs_vars))
+        return live_vars_out
+
+    def find_live_vars_from_extracted_fragment(self, py_originals, use):
+        live_vars_out = set()
+        is_abstraction_call = lambda x: isinstance(x, ast.Call) and x.func.id.startswith("fn_")
+        new_body = list(filter(lambda x:
+                               not isinstance(x, ast.ImportFrom) and not is_abstraction_call(x), use.target_ast.body))
+        target = ast.unparse(ast.Module(body=new_body, type_ignores=[]))
+        for py_orig in py_originals:
+            index = py_orig.find(target)
+            if index != -1:
+                # Found the code where the method was extracted.
+                live_vars = lisp2py.ExtractedFragment \
+                    .create_from(py_orig, target, index).find_used_vars_later()
+                live_vars_out = live_vars_out.union(live_vars)
         return live_vars_out
 
     def get_additional_params(self):
@@ -116,4 +149,5 @@ class StitchAbstraction:
 
         self.get_and_set_live_out(stitch_originals)
         abstraction_py_obj.add_return_value(abstraction_py_obj.abstraction_body_as_fndef)
-        return ast.unparse(abstraction_py_obj.abstraction_body_as_fndef)
+        self.abstraction_body_py = ast.unparse(abstraction_py_obj.abstraction_body_as_fndef)
+        return self.abstraction_body_py
